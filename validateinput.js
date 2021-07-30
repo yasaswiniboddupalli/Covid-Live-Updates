64035f072f16295b901817e717a182c1889ee061
Yash <yasaswini1205@gmail.com>
2021-07-30 12:39:24 +0100

    jest final changes

--- validateinput.js
+++ validateinput.js
@@ -1,4 +1,9 @@
-function validateInput(){
+function validateInput(country){
+    const country_name = country.value.toLowerCase();
+    const name_ver=/^[a-z]{3,}$/;
+    if(name_ver.test(country_name)){
+        return true;
+    }
 
 }
 module.exports = validateInput;
